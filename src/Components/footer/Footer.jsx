1b12b2ef514bffa92ad790c22b0c517f6fe1e9ba
Chiarads23 <chiarads2328@gmail.com>
2023-03-02 17:31:17 +0100

    footer-fix

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
         <a
           href={"/images/ChiaraDeSantisCV.jpg"}
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noopener"
         >
           Resume
         </a>
