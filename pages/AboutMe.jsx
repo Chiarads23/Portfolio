0b8039eccdca29f4101e83b0dcf8c4e799e6bb8f
Chiarads23 <chiarads2328@gmail.com>
2023-03-02 17:22:58 +0100

    text-fix

--- pages/AboutMe.jsx
+++ pages/AboutMe.jsx
@@ -10,7 +10,9 @@ import { motion } from "framer-motion";
         transition={{ duration: 0.5 }}
          className={styles.AboutMe}>
             <h3>About me</h3>
-            <h4>My coding journey started just a year ago, when  I was looking for a new path that could take out a new better version of me. And it has been a revelation so far. I've been able to make the most of my attention to details and endless curiosity to learn new ways of communication that allow people to connect.
+            <h4>My coding journey started just a year ago, when  I was looking for a new path that could take out a new better version of me.
+                 And it has been a revelation so far. I&apos;ve been able to make the most of my attention to details and endless curiosity
+                  to learn new ways of communication that allow people to connect.
             </h4>
         </motion.div>
     )
